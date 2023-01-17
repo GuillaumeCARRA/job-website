066f659ff48a415ab414ec6df0621675b4373a41
GuillaumeCARRA <carraguillaume@gmail.com>
2023-01-17 12:05:24 +0100

    added css file for navbar component

--- Front/src/pages/Home/index.js
+++ Front/src/pages/Home/index.js
@@ -1,12 +1,11 @@
 import React from 'react'; 
-
+import Navbar from '../../components/Navbar';
 import './home.css';
 
 function Home() {
   return (
     <div className='home'>
-      Home
-  
+     <Navbar />
     </div>
   )
 }

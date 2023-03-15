0d09f650c56061581bcabf288f14dcf24ea8784f
GuillaumeCARRA <carraguillaume@gmail.com>
2023-03-15 14:31:54 +0100

    setting up sequelize and expressjs

---
+++ Back/app/app.js
@@ -0,0 +1,38 @@
+require('dotenv').config(); 
+
+const express = require('express');
+
+// create server express
+const app = express(); 
+
+// app.use((req, res, next) => {
+//     res.setHeader('Content-Security-Policy', "default-src 'none'; img-src *; script-src *; style-src *;");
+//     next();
+//   });
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+  });
+
+// const router = require('./Back/app/routers'); 
+
+// //add middleware for data POST
+// app.use(express.urlencoded({extended: true}))
+
+app.use(express.json());
+
+// app.use(cors({
+//     origin: '*',
+//     methods: 'GET,POST,PATCH,DELETE,OPTIONS',
+//     allowedHeaders: 'Content-Type'
+// }));
+
+
+// app.use(router);
+
+
+// server launch
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+});

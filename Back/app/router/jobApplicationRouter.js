32e232ae37075182750032164d92f2d7789c764c
GuillaumeCARRA <carraguillaume@gmail.com>
2023-05-12 15:49:24 +0200

    import express and controller for all router file

---
+++ Back/app/router/jobApplicationRouter.js
@@ -0,0 +1,7 @@
+const express = require('express'); 
+
+const jobApplicationsController = require('../controllers/jobApplicationsController');
+
+const router = express.Router();
+
+module.exports = router; 
---
+++ Back/app/router/recruiterRouter.js
@@ -0,0 +1,7 @@
+const express = require('express'); 
+
+const recruiterController = require('../controllers/recruiterController');
+
+const router = express.Router();
+
+module.exports = router; 

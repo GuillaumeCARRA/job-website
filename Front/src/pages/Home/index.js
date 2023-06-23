385a675c6b3bed640d32b94d9f67c22b41224a1b
GuillaumeCARRA <carraguillaume@gmail.com>
2023-06-23 17:42:34 +0200

    add css style for homepage

--- Front/src/pages/Home/index.js
+++ Front/src/pages/Home/index.js
@@ -11,14 +11,32 @@ function Home() {
             <h1 
               className='home__title'
             >
-              Trouver votre emploi à distance en France grâce à <span className='home_title--purple'>DistoJob</span>
+              Trouver votre emploi à distance en France grâce à <span>DistoJob</span>
             </h1>
           </div>
           <div className='home__totaljob'>
             <span className='home__numberjob'>500 offres d'emplois disponibles en ce moment</span>
           </div>
           <div className='home_search'>
-
+            <div className='home_search__input'>
+              <input
+                className='home_search__input-field'
+                type='text'
+                placeholder='Rechercher un métier'
+              />
+            </div>
+            <div className='home_search__input'>
+              <input
+                className='home_search__input-field home_search__input-field--border'
+                type='text'
+                placeholder='Rechercher une ville'
+              />
+            </div>
+            <button
+              className='home_search__btn'
+            >
+              Rechercher
+            </button>
           </div>
         </div>
     </div>

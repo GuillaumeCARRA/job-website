f8f3069fe43846b3649ddc0ecac0a9358d45f645
GuillaumeCARRA <carraguillaume@gmail.com>
2023-06-23 16:26:11 +0200

    setting up home page

--- Front/src/pages/Home/index.js
+++ Front/src/pages/Home/index.js
@@ -7,7 +7,19 @@ function Home() {
     <div className='home'>
         <Navbar />
         <div className='home__container'>
-          hello
+          <div className='home__bigtitle'> 
+            <h1 
+              className='home__title'
+            >
+              Trouver votre emploi à distance en France grâce à <span className='home_title--purple'>DistoJob</span>
+            </h1>
+          </div>
+          <div className='home__totaljob'>
+            <span className='home__numberjob'>500 offres d'emplois disponibles en ce moment</span>
+          </div>
+          <div className='home_search'>
+
+          </div>
         </div>
     </div>
   )

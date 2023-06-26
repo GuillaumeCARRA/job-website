6d646bed02a2850fc04da79b5efe831a5e74efcc
GuillaumeCARRA <carraguillaume@gmail.com>
2023-06-26 13:51:25 +0200

    add cards for homepage

--- Front/src/pages/Home/index.js
+++ Front/src/pages/Home/index.js
@@ -7,38 +7,125 @@ function Home() {
     <div className='home'>
         <Navbar />
         <div className='home__container'>
-          <div className='home__bigtitle'> 
-            <h1 
-              className='home__title'
-            >
-              Trouver votre emploi à distance en France grâce à <span>DistoJob</span>
-            </h1>
-          </div>
-          <div className='home__totaljob'>
-            <span className='home__numberjob'>500 offres d'emplois disponibles en ce moment</span>
-          </div>
-          <div className='home_search'>
-            <div className='home_search__input'>
-              <input
-                className='home_search__input-field'
-                type='text'
-                placeholder='Rechercher un métier'
-              />
+          <section>
+            <div className='home__bigtitle'> 
+              <h1 
+                className='home__title'
+              >
+                Trouver votre emploi à distance en France grâce à <span>DistoJob</span>
+              </h1>
             </div>
-            <div className='home_search__input'>
-              <input
-                className='home_search__input-field home_search__input-field--border'
-                type='text'
-                placeholder='Rechercher une ville'
-              />
+            <div className='home__totaljob'>
+              <span className='home__numberjob'>500 offres d'emplois disponibles en ce moment</span>
             </div>
-            <button
-              className='home_search__btn'
-            >
-              Rechercher
-            </button>
-          </div>
+            <div className='home_search'>
+              <div className='home_search__input'>
+                <input
+                  className='home_search__input-field'
+                  type='text'
+                  placeholder='Rechercher un métier'
+                />
+              </div>
+              <div className='home_search__input'>
+                <input
+                  className='home_search__input-field home_search__input-field--border'
+                  type='text'
+                  placeholder='Rechercher une ville'
+                />
+              </div>
+              <button
+                className='home_search__btn'
+              >
+                Rechercher
+              </button>
+            </div>
+          </section>
+          <section>
+            <div className='home__solutions'>
+              <div className='home__titlesolution'>
+                <h2 className='home__subtitle'>
+                  Une Plateforme Plusieurs <span>Catégorie</span>
+                </h2>
+              </div>
+                <div className='home__cards'>
+                  <div className='home__card'>
+                    <div className='home__card-icon'>
+                      <i className='fa fa-icon'>test</i> {/* Remplacez "fa-icon" par la classe d'icône souhaitée */}
+                    </div>
+                    <div className='home__card-content'>
+                      <h3 className='home__card-title'>Titre de la carte</h3>
+                      <p className='home__card-description'>Description de la carte</p>
+                    </div>
+                  </div>
+                  <div className='home__card'>
+                    <div className='home__card-icon'>
+                      <i className='fa fa-icon'>test</i> {/* Remplacez "fa-icon" par la classe d'icône souhaitée */}
+                    </div>
+                    <div className='home__card-content'>
+                      <h3 className='home__card-title'>Titre de la carte</h3>
+                      <p className='home__card-description'>Description de la carte</p>
+                    </div>
+                  </div>
+                  <div className='home__card'>
+                    <div className='home__card-icon'>
+                      <i className='fa fa-icon'>test</i> {/* Remplacez "fa-icon" par la classe d'icône souhaitée */}
+                    </div>
+                    <div className='home__card-content'>
+                      <h3 className='home__card-title'>Titre de la carte</h3>
+                      <p className='home__card-description'>Description de la carte</p>
+                    </div>
+                  </div>
+                  <div className='home__card'>
+                    <div className='home__card-icon'>
+                      <i className='fa fa-icon'>test</i> {/* Remplacez "fa-icon" par la classe d'icône souhaitée */}
+                    </div>
+                    <div className='home__card-content'>
+                      <h3 className='home__card-title'>Titre de la carte</h3>
+                      <p className='home__card-description'>Description de la carte</p>
+                    </div>
+                  </div>
+                  <div className='home__card'>
+                    <div className='home__card-icon'>
+                      <i className='fa fa-icon'>test</i> {/* Remplacez "fa-icon" par la classe d'icône souhaitée */}
+                    </div>
+                    <div className='home__card-content'>
+                      <h3 className='home__card-title'>Titre de la carte</h3>
+                      <p className='home__card-description'>Description de la carte</p>
+                    </div>
+                  </div>
+                  <div className='home__card'>
+                    <div className='home__card-icon'>
+                      <i className='fa fa-icon'>test</i> {/* Remplacez "fa-icon" par la classe d'icône souhaitée */}
+                    </div>
+                    <div className='home__card-content'>
+                      <h3 className='home__card-title'>Titre de la carte</h3>
+                      <p className='home__card-description'>Description de la carte</p>
+                    </div>
+                  </div>
+                  <div className='home__card'>
+                    <div className='home__card-icon'>
+                      <i className='fa fa-icon'>test</i> {/* Remplacez "fa-icon" par la classe d'icône souhaitée */}
+                    </div>
+                    <div className='home__card-content'>
+                      <h3 className='home__card-title'>Titre de la carte</h3>
+                      <p className='home__card-description'>Description de la carte</p>
+                    </div>
+                  </div>
+                  <div className='home__card'>
+                    <div className='home__card-icon'>
+                      <i className='fa fa-icon'>test</i> {/* Remplacez "fa-icon" par la classe d'icône souhaitée */}
+                    </div>
+                    <div className='home__card-content'>
+                      <h3 className='home__card-title'>Titre de la carte</h3>
+                      <p className='home__card-description'>Description de la carte</p>
+                    </div>
+                  </div>
+                </div>
+            </div>
+
+          </section>
         </div>
+       
     </div>
   )
 }
